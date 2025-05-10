ca2a1be2650dfafb91679ad6026fa3c5642648cd
Varshil <medidhiv@msu.edu>
2025-05-09 21:41:42 -0400

    added header strings and comments

--- graphs/constructor.py
+++ graphs/constructor.py
@@ -86,32 +86,47 @@ class  Graph:
         for vertex in self.adj_list:
             print(vertex, ":" , self.adj_list[vertex])
     def add_vertex(self,vertex):
-        if vertex not in self.adj_list:
-            self.adj_list[vertex]=[]
-            return True
-        return False
+        '''
+        adding a new vertex to the graph using adj list
+        '''
+        if vertex not in self.adj_list: # check if the vertex is not already present
+            self.adj_list[vertex]=[] # if not add the vertex with an empty list
+            return True # return True showing the sign of adding a vertex to the matrix
+        return False # return false if the vertex is already in the matrix
     def add_edge(self,v1,v2):
-        if v1 in self.adj_list and v2 in self.adj_list:
-            self.adj_list[v1].append(v2)
-            self.adj_list[v2].append(v1)
-            return True
-        return False
+        '''
+        adding a new edges  to the given vertex's  using adj list and taking into the account that the vertices are already present 
+        and the vertices doesn't have any edges yet.
+        '''
+        if v1 in self.adj_list and v2 in self.adj_list: # edge case check : check if the vertex is already in the matrix and if it is 
+            self.adj_list[v1].append(v2) # then add edge of v2 to v1
+            self.adj_list[v2].append(v1) # same to v2
+            return True  # return True showing the sign of the matrix has been added
+        return False # return False if any of the given vertex is not in then matrix
     def remove_edge(self,v1,v2):
-        if v1 in self.adj_list and v2 in self.adj_list:
-            try:
+        '''
+        removing edge between two vertices. 
+        '''
+        if v1 in self.adj_list and v2 in self.adj_list:  # checking if both of the matrices are in the adj list if yes 
+            try: # try removing those edges
                 self.adj_list[v1].remove(v2)
                 self.adj_list[v2].remove(v1)
-            except ValueError:
-                pass
-            return True
-        return False
+            except ValueError: # if those values doesn't have any edge 
+                return False # return false
+            return True # return True if no error has occured
+        return False # return False otherwise
     def remove_vertex(self,v):
-        if v in self.adj_list:
-            for vertices in self.adj_list[v]:
-                self.adj_list[vertices].remove(v)
-            del self.adj_list[v]
-            return True
-        return False
+        '''
+        removing a vertex from the matrix , this is the one where we remove all the edges with the given vertex 
+        and then remove the vertex from our list
+        '''
+        if v in self.adj_list:  # checking if the given vertex already in the list
+            for vertices in self.adj_list[v]: # going for every value which is with the edge with  the vertices given  
+                self.adj_list[vertices].remove(v) # and going into our dictionary using that vertice as our key and removing the given vertices as a edge
+            del self.adj_list[v] # after completion of the for loop , we should delete the value v
+            return True # return True if everything goes well 
+        return False # return False
+
 my_graph=Graph()
 
 my_graph.add_vertex('a')
